365386105af457019483c94a076745a5eedffa25
Akhnovskaya Yuliya <ahnovskaya.ju@mail.ru>
2023-11-14 15:37:19 +0300

    Do the first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+	if (discriminant < 0) {
+		console.log(arr);
+	} else if (discriminant === 0) {
+		let x = -b / (2 * a);
+		arr.push(x);
+	} else {
+		let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(x1, x2);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentEveryMonth = percent / 100 / 12;
+	let creditAmount = amount - contribution;
+	let paymentEveryMonth = creditAmount * (percentEveryMonth + (percentEveryMonth / (((1 + percentEveryMonth) ** countMonths) - 1)));
+	let totalPayment = paymentEveryMonth * countMonths;
+	return +totalPayment.toFixed(2);
 }
