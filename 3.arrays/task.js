bc79b5a0e680a4423dcf806945e892b35c50497e
Akhnovskaya Yuliya <ahnovskaya.ju@mail.ru>
2023-11-20 15:54:58 +0300

    Do the third task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	return arr1.every((item, index) => item === arr2[index])
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age) => acc + age, 0) / users.filter(user => user.gender === gender).length;
+	if (users.filter(user => user.gender === gender).length === 0) {
+		return 0;
+	} else {
+		return result;
+	}
 }
