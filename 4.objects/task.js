9b72a5b4fc91f2a606f5add5feea17a770aa978f
Akhnovskaya Yuliya <ahnovskaya.ju@mail.ru>
2023-11-22 14:24:42 +0300

    Do the 4 task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+}
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (!this.marks) {
+		console.log("Студент отчислен");
+		return;
+	}
+	this.marks.push(...marks);
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	} else {
+		let averageMark = this.marks.reduce((acc, item) => acc + item) / this.marks.length;
+		return averageMark;
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
 
-Student.prototype.exclude = function (reason) {
-  
-}
+
